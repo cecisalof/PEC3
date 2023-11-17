e529d2fec2ab1cca7cdf83e9a20368f8ebfe59e6
Cecilia <cecilia.sanchezlosada@gmail.com>
2023-11-17 13:06:55 +0100

    [Ej3]: d

--- PEC3_Ej3/src/ejercicio3.ts
+++ PEC3_Ej3/src/ejercicio3.ts
@@ -1,10 +1,10 @@
-Sustituye /***/ por las instrucciones adecuadas que cumplan las operaciones 
-y salidas indicadas en los comentarios.
+//Sustituye /***/ por las instrucciones adecuadas que cumplan las operaciones 
+//y salidas indicadas en los comentarios.
 
 abstract class Animal {
     static population: number = 0;
     constructor() {
-        /***/
+        Animal.population++;
     }
     public abstract sound(): void;
 }
@@ -12,20 +12,35 @@ abstract class Animal {
 class Dog extends Animal {
     color: string;
 
-    /***/
+    constructor(color: string) {
+        super();
+        this.color = color;
+    }
 
     public iamadog() {
         console.log('yes, this is a dog');
+    }
+
+    public sound() {
+        console.log('WOW');
     }
 }
 
 class Cat extends Animal {
     gender: string;
     
-    /***/
+    
+    constructor(gender: string) {
+        super();
+        this.gender = gender;
+    }
 
     public iamacat() {
         console.log('yes, this is a cat');
+    }
+
+    public sound() {
+        console.log('MEOW');
     }
 }
 
@@ -36,9 +51,12 @@ animals.push(new Cat('female'));
 animals.push(new Dog('black'));
 
 for(let animal of animals){
-    /***/
-    
-
+    animal.sound();
+    if (animal instanceof Dog) {
+        (animal as Dog).iamadog();
+    } else if (animal instanceof Cat) {
+        (animal as Cat).iamacat();
+    }
 }
 /**  loop prints these lines
 MEOW
